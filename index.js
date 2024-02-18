662fcbf3940088268f80d2ad59190372ceb5e3cb
SebastianH03 <sebasht2003@gmail.com>
2024-02-18 17:00:35 -0500

    Crear el servidor Node

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
+//Dependencias
 const { connection } = require("./database/connection");
+const express = require("express");
+const cors = require("cors");
 
 // Iniciar la base de datos
 console.log("App de Node iniciada")
 
 // Conectar la base de datos
 connection();
+
+// Crear servidor Node (para escuchar peticiones http y crear rutas) Express es el framework
+const app = express();
+const puerto = 3900;
+
+// Configurar cors (middleware antes de ejecutar una ruta para evitar problemas de dominio)
+app.use(cors());
+
+// Convertir body a objeto js. Parsear los datos a un objeto js.
+app.use(express.json());
+
+// Crear rutas
+
+// Crear servidor y escuchar peticiones http
+app.listen(puerto, ()=> {
+    console.log("Servidor corriendo en el puerto "+puerto);
+})
