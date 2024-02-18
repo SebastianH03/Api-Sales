5f7da58be50ac04cce70474ba2093e9f0e757cde
SebastianH03 <sebasht2003@gmail.com>
2024-02-18 17:43:19 -0500

    Definimos arquitectura mvc y creamos el primer modelo para Stock

---
+++ models/Stock.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose"); //Esquemas y modelos para trabajar con los documentos de la colección Stock.
+
+//Esquema del producto
+const ProductSchema = Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    product_name: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: [String],
+        default: ["None"] //Si no se especifica su categoría es None
+    }
+})
+
+//Esquema del Stock
+const StockSchema = Schema({
+    id: {
+        type: String,
+        required: true
+    },
+    product: ProductSchema,
+    quantity: {
+        type: Number,
+        required: true
+    }
+}) //Esquema
+
+module.exports = model("Stock", StockSchema, "stock")
+                    // Primero va el nombre referido, luego el esquema para importar y finalmente la colección que se tendrá en cuenta
