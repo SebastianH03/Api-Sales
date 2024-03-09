095686402fa4d550698a25b0d93fd419e9567642
SebastianH03 <sebasht2003@gmail.com>
2024-03-09 16:04:03 -0500

    Actualización de proveedores

--- Api-sales/models/Stock.js
+++ Api-sales/models/Stock.js
@@ -1,11 +1,29 @@
 const { Schema, model } = require("mongoose");
 
 
-const StockSchema = Schema({
-    product: {
+const ProductSchema = Schema({
+    name:{
         type: String,
         required: true
     },
+    price:{
+        type: Number,
+        required: true
+    },
+    tags:{
+        type:[String],
+        default:[]
+    },
+    provider:{
+        type: Schema.Types.ObjectId,
+        ref: 'Suppliers' //Referencia a la colección de proveedores
+    }
+})
+
+
+
+const StockSchema = Schema({
+    product: ProductSchema,
     quantity: {
         type: Number,
         required: true
